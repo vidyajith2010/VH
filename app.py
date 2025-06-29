445e9aaf20d666b4f11cd29b331ec65d53de5db7
Vidya Chandran G <69622553+vidyajith2010@users.noreply.github.com>
2025-06-29 12:26:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def video_feed():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render uses PORT env variable
+    app.run(host='0.0.0.0', port=port)
